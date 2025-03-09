1904d3e1b35eb33af4954e502b8c48557df401a5
Carlos563429 <carlitosfg.5051@gmail.com>
2025-03-08 23:31:35 -0600

    [🍪] Commit: Minor changes

--- thumbnail/urls.py
+++ thumbnail/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import test_view
+from .views import get_thumbnail
 
 urlpatterns = [
-    path('test/', test_view),
+    path('thumbnail/', get_thumbnail),
+    path('/', test_view),
 ]
--- thumbnail/views.py
+++ thumbnail/views.py
@@ -1,3 +1,5 @@
+from pytube import YouTube
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,3 +9,14 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def test_view(request):
     return Response({'Message': 'Hello CatWorld! 🐈‍⬛'})
+
+# TO DO: Implement get_thumbnail
+@api_view(['GET'])
+def get_thumbnail(request):
+    """Return a video info"""
+
+    url = request.GET.get('url')
+    if not url:
+        return Response({'error': 'Missing URL parameter'}, status=400)
+    
+    return Response(url)
