000d17dd89d92342eeab7db96d3b7708fa58f5b5
Carlos563429 <carlitosfg.5051@gmail.com>
2025-03-12 22:08:37 -0600

    [🚀] Feature: Add get info video function

--- download_video/urls.py
+++ download_video/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import download_video
+from .views import get_info_video
 
 urlpatterns = [
     path('download/', download_video)
+    path('download/', get_info_video)
 ]
--- download_video/views.py
+++ download_video/views.py
@@ -1,42 +1,45 @@
 import io
 import ffmpeg
-from django.http import FileResponse, StreamingHttpResponse
+from django.http import FileResponse, JsonResponse, StreamingHttpResponse
 from yt_dlp import YoutubeDL
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view(['GET'])
-def download_video(request):
-    """Download the video on the given URL"""
+def get_info_video(request):
+    """Get the information of the video on the given URL"""
 
-    url = request.GET.get('url')
-    if not url:
+    URL = request.GET.get('url')
+    if not URL:
         return Response({'error': 'Missing URL parameter'}, status=400)
+
+    FORMAT = request.GET.get('format')
+    if not FORMAT:
+        # return Response({'error': 'Missing URL parameter'}, status=400) TODO: UNCOMMENT THIS LINE
+        FORMAT = 'bestvideo+bestaudio/best' # TODO: REMOVE THIS LINE
 
     try:
         
         OPTIONS = {
-            'format': 'bestvideo+bestaudio/best',
+            'format': FORMAT,
             'outtmpl': '%(title)s.%(ext)s',
             'merge_output_format': 'mp4', 
         }
 
-        buffer = io.BytesIO()
-
         # Get the video
         with YoutubeDL(OPTIONS) as ydl:
-            buffer.write(ydl.extract_info(url, download=True))
+            video_info = ydl.extract_info(URL, download=True)
+            print(video_info)
 
-        buffer.seek(0)
-        print(buffer)
-
-        # Return response
-        response_video = StreamingHttpResponse(buffer, content_type='video/mp4')
-        response_video['Content-Disposition'] = f'attachment'
-        return response_video
+        return JsonResponse(video_info, status=200)
 
     except Exception as e:
         import traceback
         traceback.print_exc()
         return Response({'Unexpected error': str(e)}, status=500)
+
+    
+@api_view(['GET'])
+def download_video(request):
+    """Download the video on the given URL"""
