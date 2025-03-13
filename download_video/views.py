611de1338f20be54186035f2a0c48c41f1feae55
Carlos563429 <carlitosfg.5051@gmail.com>
2025-03-12 21:53:52 -0600

    [🐍] Commit: Checkpoint for unsolved bugs

--- download_video/views.py
+++ download_video/views.py
@@ -1,4 +1,5 @@
 import io
+import ffmpeg
 from django.http import FileResponse, StreamingHttpResponse
 from yt_dlp import YoutubeDL
 from rest_framework.response import Response
@@ -17,34 +18,25 @@ def download_video(request):
         
         OPTIONS = {
             'format': 'bestvideo+bestaudio/best',
-            'outtmpl': 'media/%(title)s.%(ext)s',
+            'outtmpl': '%(title)s.%(ext)s',
             'merge_output_format': 'mp4', 
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192'
-            }]
         }
+
+        buffer = io.BytesIO()
 
         # Get the video
         with YoutubeDL(OPTIONS) as ydl:
-            stream = ydl.extract_info(url, download=True)
+            buffer.write(ydl.extract_info(url, download=True))
 
-        video_filename = f"media/{stream['title']}.mp4"
-
-        def file_iterator(filename, chunk_size=8192):
-            with open(filename, "rb") as f:
-                while chunk := f.read(chunk_size):
-                    yield chunk
-
+        buffer.seek(0)
+        print(buffer)
 
         # Return response
-        response_video = StreamingHttpResponse(file_iterator(video_filename), content_type='video/mp4')
+        response_video = StreamingHttpResponse(buffer, content_type='video/mp4')
         response_video['Content-Disposition'] = f'attachment'
         return response_video
 
     except Exception as e:
         import traceback
-        print("🔥 ERROR EN DESCARGA 🔥")
         traceback.print_exc()
         return Response({'Unexpected error': str(e)}, status=500)
